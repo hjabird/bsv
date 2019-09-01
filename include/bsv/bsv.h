b8ff4c221e3d4aa3cbf70284e8c6db3baf143a66
HJA Bird <h.bird.1@research.gla.ac.uk>
2019-09-01 22:47:56 +0100

    Add Mat4D

--- include/bsv/bsv.h
+++ include/bsv/bsv.h
@@ -27,6 +27,7 @@ SOFTWARE.
 ============================================================================*/
 
 /* Vectors -------------*/
+
 typedef struct bsv_V4f{
 	float x[4];
 } bsv_V4f;
@@ -52,6 +53,15 @@ typedef struct bsv_V2d{
 } bsv_V2d;
 
 /* Matrices ------------*/
+
+typedef struct bsv_M4f {
+	float x[4][4];
+} bsv_M4f;
+
+typedef struct bsv_M4d {
+	double x[4][4];
+} bsv_M4d;
+
 typedef struct bsv_M3f{
 	float x[3][3];
 } bsv_M3f;
@@ -150,6 +160,28 @@ static inline int bsv_V2d_isnequal(const bsv_V2d self, const bsv_V2d other);
 static inline bsv_V2f bsv_V2d_toV2f(const bsv_V2d input);
 
 /* Matrix functions ========================================================*/
+
+static inline int bsv_M4f_isequal(const bsv_M4f self, const bsv_M4f other);
+
+static inline bsv_M4d bsv_M4d_zero(void);
+static inline bsv_M4d bsv_M4d_ones(void);
+static inline bsv_M4d bsv_M4d_identity(void);
+static inline bsv_M4d bsv_M4d_rotation(const double rad_angle, const int axis);
+static inline bsv_M4d bsv_M4d_scale(const double scale_factor);
+static inline bsv_M4d bsv_M4d_plus(const bsv_M4d self, const bsv_M4d other);
+static inline bsv_M4d bsv_M4d_minus(const bsv_M4d self, const bsv_M4d other);
+static inline bsv_M4d bsv_M4d_uminus(const bsv_M4d self);
+static inline bsv_M4d bsv_M4d_mult(const bsv_M4d self, const double multipler);
+static inline bsv_M4d bsv_M4d_div(const bsv_M4d self, const double div);
+static inline bsv_V4d bsv_V4d_vmult(const bsv_M4d self, const bsv_V4d other);
+static inline bsv_M4d bsv_M4d_mmult(const bsv_M4d self, const bsv_M4d other);
+static inline double bsv_M4d_norm(const bsv_M4d self);
+static inline double bsv_M4d_det(const bsv_M4d self);
+static inline bsv_M4d bsv_M4d_inv(const bsv_M4d self);
+static inline bsv_M4d bsv_M4d_transpose(const bsv_M4d self);
+static inline int bsv_M4d_isequal(const bsv_M4d self, const bsv_M4d other);
+static inline int bsv_M4d_isnequal(const bsv_M4d self, const bsv_M4d other);
+static inline bsv_M4f bsv_M4d_toM4f(const bsv_M4d self);
 
 static inline bsv_M3d bsv_M3d_zero(void);
 static inline bsv_M3d bsv_M3d_ones(void);
@@ -922,6 +954,344 @@ bsv_V2f bsv_V2d_toV2f(const bsv_V2d input)
 
 /* Matrix methods implementation ===========================================*/
 
+/* M4f ---------------------------------------------------------------------*/
+
+static inline int bsv_M4f_isequal(const bsv_M4f self, const bsv_M4f other)
+{
+	int retv;
+	if (memcmp((void*)& self, (void*)& other, sizeof(bsv_M4f)) == 0)
+	{
+		retv = 1;
+	}
+	else {
+		retv = 0;
+	}
+	return retv;
+}
+
+/* M4d ---------------------------------------------------------------------*/
+
+static inline bsv_M4d bsv_M4d_zero(void)
+{
+	bsv_M4d ret = { 0., 0., 0., 0.,
+					0., 0., 0., 0.,
+					0., 0., 0., 0.,
+					0., 0., 0., 0. };
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_ones(void)
+{
+	bsv_M4d ret = { 1., 1., 1., 1.,
+					1., 1., 1., 1.,
+					1., 1., 1., 1.,
+					1., 1., 1., 1. };
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_identity(void)
+{
+	bsv_M4d ret = { 1., 0., 0., 0.,
+					0., 1., 0., 0.,
+					0., 0., 1., 0.,
+					0., 0., 0., 1. };
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_rotation(
+	const double rad_angle, const int axis)
+{
+
+	assert(axis >= 0);	/* axis 0 is X */
+	assert(axis < 4);	/* axis 3 is W */
+	bsv_M4d ret = bsv_M4d_identity();
+	double c = cos(rad_angle), s = sin(rad_angle);
+	switch (axis) {
+	case 0:
+		ret.x[1][1] = c;
+		ret.x[2][2] = c;
+		ret.x[1][2] = -s;
+		ret.x[2][1] = s;
+		break;
+	case 1:
+		ret.x[0][0] = c;
+		ret.x[2][2] = c;
+		ret.x[0][2] = s;
+		ret.x[2][0] = -s;
+		break;
+	case 2:
+		ret.x[0][0] = c;
+		ret.x[1][1] = c;
+		ret.x[0][1] = -s;
+		ret.x[1][0] = s;
+		break;
+	}
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_scale(const double scale_factor)
+{
+	bsv_M4d ret = { scale_factor, 0., 0., 0.,
+					0., scale_factor, 0., 0.,
+					0., 0., scale_factor, 0.,
+					0., 0., 0., scale_factor };
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_plus(const bsv_M4d self, const bsv_M4d other)
+{
+	bsv_M4d ret;
+	int i;
+	for (i = 0; i < 4; ++i) {
+		ret.x[i][0] = self.x[i][0] + other.x[i][0];
+		ret.x[i][1] = self.x[i][1] + other.x[i][1];
+		ret.x[i][2] = self.x[i][2] + other.x[i][2];
+		ret.x[i][3] = self.x[i][3] + other.x[i][3];
+	}
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_minus(const bsv_M4d self, const bsv_M4d other)
+{
+	bsv_M4d ret;
+	int i;
+	for (i = 0; i < 4; ++i) {
+		ret.x[i][0] = self.x[i][0] - other.x[i][0];
+		ret.x[i][1] = self.x[i][1] - other.x[i][1];
+		ret.x[i][2] = self.x[i][2] - other.x[i][2];
+		ret.x[i][3] = self.x[i][3] - other.x[i][3];
+	}
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_uminus(const bsv_M4d self) {
+	bsv_M4d ret;
+	int i;
+	for (i = 0; i < 4; ++i) {
+		ret.x[i][0] = -self.x[i][0];
+		ret.x[i][1] = -self.x[i][1];
+		ret.x[i][2] = -self.x[i][2];
+		ret.x[i][3] = -self.x[i][3];
+	}
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_mult(const bsv_M4d self, const double mult)
+{
+	bsv_M4d ret;
+	int i;
+	for (i = 0; i < 4; ++i) {
+		ret.x[i][0] = self.x[i][0] * mult;
+		ret.x[i][1] = self.x[i][1] * mult;
+		ret.x[i][2] = self.x[i][2] * mult;
+		ret.x[i][3] = self.x[i][3] * mult;
+	}
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_div(const bsv_M4d self, const double divisor)
+{
+	bsv_M4d ret;
+	int i;
+	for (i = 0; i < 4; ++i) {
+		ret.x[i][0] = self.x[i][0] / divisor;
+		ret.x[i][1] = self.x[i][1] / divisor;
+		ret.x[i][2] = self.x[i][2] / divisor;
+		ret.x[i][3] = self.x[i][3] / divisor;
+	}
+	return ret;
+}
+
+static inline bsv_V4d bsv_M4d_vmult(const bsv_M4d self, const bsv_V4d other)
+{
+	bsv_V4d ret;
+	int i;
+	for (i = 0; i < 4; ++i) {
+		ret.x[i] =	self.x[i][0] * other.x[0] +
+					self.x[i][1] * other.x[1] +
+					self.x[i][2] * other.x[2] +
+					self.x[i][3] * other.x[3];
+	}
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_mmult(
+	const bsv_M4d self, const bsv_M4d other)
+{
+	bsv_M4d ret;
+	int i, j;
+	for (i = 0; i < 4; ++i) {
+		for (j = 0; j < 4; ++j) {
+			ret.x[i][j] =	self.x[i][0] * other.x[0][j] +
+							self.x[i][1] * other.x[1][j] +
+							self.x[i][2] * other.x[2][j] +
+							self.x[i][3] * other.x[3][j];
+		}
+	}
+	return ret;
+}
+
+static inline double bsv_M4d_norm(const bsv_M4d self)
+{
+	double ret = 0.;
+	int i, j;
+	for (i = 0; i < 4; ++i) {
+		for (j = 0; j < 4; ++j) {
+			ret += self.x[i][j] * self.x[i][j];
+		}
+	}
+	return sqrt(ret);
+}
+
+static inline double bsv_M4d_det(const bsv_M4d self)
+{
+	double ret = 0.;
+	bsv_M4d cpy = self;	/* So we can modify the matrix. */
+	int i, j, k;
+	double diagv, rowmult;
+	/*	RULES:
+		- Swapping rows or columns multiplies det by -1.
+		- Adding multiples of rows to each other have no effect.
+		- The trace of a triangular matrix is the determinant.
+	*/
+	/* First convert to triangular form using Gaussian elimination */
+	for (i = 0; i < 4; ++i) {
+		diagv = cpy.x[i][i];
+		if (diagv == 0.) {
+			/* Add some other lower row to this one such that its non-zero 
+			on the diagonal. */
+			for (j = i; j < 4; ++j) {
+				if (cpy.x[j][i] != 0.) { break; }
+			}
+			/* If the diagonal and all rows below are zero, the det is zero.*/
+			if (j == 4) { return 0.; }	/* ### PRE-MATURE FUNCTION EXIT ### */
+			for (k = i; k < 4; ++k) {	/* Prior to i, all entries are zero.*/
+				cpy.x[i][k] += cpy.x[j][k];
+			}
+			diagv = cpy.x[i][i];
+		}
+		/* Now modify lower rows to zero in col i using our pivot. */
+		for (j = i + 1; j < 4; ++j) {
+			rowmult = cpy.x[j][i] / diagv;
+			/* Optimisation guess: better to avoid loop. */
+			cpy.x[j][0] -= rowmult * cpy.x[i][0];
+			cpy.x[j][1] -= rowmult * cpy.x[i][1];
+			cpy.x[j][2] -= rowmult * cpy.x[i][2];
+			cpy.x[j][3] -= rowmult * cpy.x[i][3];
+		}
+	}
+	ret = cpy.x[0][0] * cpy.x[1][1] * cpy.x[2][2] * cpy.x[3][3];
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_inv(const bsv_M4d self)
+{
+	bsv_M4d ret = bsv_M4d_identity();
+	bsv_M4d cpy = self;
+	/* Gauss-Jordan 
+	We can:
+		- swap rows
+		- add multiples of rows to each other
+		- Multiply rows by non-zero scalars
+	until we obtain the identity matrix in cpy and inv in ret	*/
+	/* Part 1: Obtain upper triangular matrix. */
+	int i, j, k;
+	double diagv, rowmult;
+	/* Make lower triangular. */
+	for (i = 0; i < 4; ++i) {
+		diagv = cpy.x[i][i];
+		if (diagv == 0.) {
+			/* Add some other lower row to this one such that its non-zero
+			on the diagonal. We just hope when it comes to the last row. */
+			for (j = i; j < 3; ++j) {
+				if (cpy.x[j][i] != 0.) { break; }
+			}
+			for (k = 0; k < 4; ++k) {
+				cpy.x[i][k] += cpy.x[j][k];
+				ret.x[i][k] += ret.x[j][k];
+			}
+			diagv = cpy.x[i][i];
+		}
+		/* Now modify lower rows to be zero in col i using our pivot. */
+		for (j = i + 1; j < 4; ++j) {
+			rowmult = cpy.x[j][i] / diagv;
+			/* Optimisation guess: better to avoid loop. */
+			cpy.x[j][0] -= rowmult * cpy.x[i][0];
+			cpy.x[j][1] -= rowmult * cpy.x[i][1];
+			cpy.x[j][2] -= rowmult * cpy.x[i][2];
+			cpy.x[j][3] -= rowmult * cpy.x[i][3];
+			ret.x[j][0] -= rowmult * ret.x[i][0];
+			ret.x[j][1] -= rowmult * ret.x[i][1];
+			ret.x[j][2] -= rowmult * ret.x[i][2];
+			ret.x[j][3] -= rowmult * ret.x[i][3];
+		}
+	}
+	/* Now, normalise each row to have zero on the diagonal. */
+	for (i = 0; i < 4; ++i) {
+		rowmult = 1./cpy.x[i][i];
+		for (j = 0; j < 4; ++j) {
+			cpy.x[i][j] *= rowmult;
+			ret.x[i][j] *= rowmult;
+		}
+	}
+	/* Now using the unit diagonal we can assume that cpy becomes
+	the identity matrix and modify only ret to complete the inversion. */
+	for (i = 3; i > 0; --i) {
+		for (j = 0; j < i; ++j) {
+			rowmult = cpy.x[j][i];
+			for (k = 0; k < 4; ++k) {
+				ret.x[j][k] -= rowmult * ret.x[i][k];
+			}
+		}
+	}
+	/* And we're done! */
+	return ret;
+}
+
+static inline bsv_M4d bsv_M4d_transpose(const bsv_M4d self)
+{
+	bsv_M4d ret;
+	int i;
+	for (i = 0; i < 4; ++i) {
+		ret.x[i][0] = self.x[0][i];
+		ret.x[i][1] = self.x[1][i];
+		ret.x[i][2] = self.x[2][i];
+		ret.x[i][3] = self.x[3][i];
+	}
+	return ret;
+}
+
+static inline int bsv_M4d_isequal(const bsv_M4d self, const bsv_M4d other)
+{
+	int retv;
+	if (memcmp((void*)& self, (void*)& other, sizeof(bsv_M4d)) == 0)
+	{
+		retv = 1;
+	}
+	else {
+		retv = 0;
+	}
+	return retv;
+}
+
+static inline int bsv_M4d_isnequal(const bsv_M4d self, const bsv_M4d other)
+{
+	return !bsv_M4d_isequal(self, other);
+}
+
+static inline bsv_M4f bsv_M4d_toM4f(const bsv_M4d self)
+{
+	bsv_M4f ret;
+	int i;
+	for (i = 0; i < 4; ++i) {
+		ret.x[i][0] = (float)self.x[i][0];
+		ret.x[i][1] = (float)self.x[i][1];
+		ret.x[i][2] = (float)self.x[i][2];
+		ret.x[i][3] = (float)self.x[i][3];
+	}
+	return ret;
+}
+
 /* M3d ---------------------------------------------------------------------*/
 
 static inline bsv_M3d bsv_M3d_zero(void)
@@ -955,7 +1325,7 @@ static inline bsv_M3d bsv_M3d_rotation(
 	assert(axis >= 0); /* axis 0 is X */
 	assert(axis < 3); /* axis 2 is Z */
 	bsv_M3d ret = bsv_M3d_identity();
-	float c = cos(rad_angle), s = sin(rad_angle);
+	double c = cos(rad_angle), s = sin(rad_angle);
 	switch (axis) {
 	case 0:
 		ret.x[1][1] = c;
